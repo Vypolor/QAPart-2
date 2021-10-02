5c151d1ac26cee7a15106955e086404d268328c1
soboi <soboiev86rus@mail.ru>
2021-10-02 13:45:34 +0400

    Lab 6-9 complete refactored v1.0

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,3 @@
-import com.sun.org.apache.bcel.internal.Const;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
@@ -6,9 +5,7 @@ import org.apache.poi.ss.usermodel.Row;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
@@ -19,103 +16,55 @@ public class Calculator {
     private static double calculatedN0 = 0.0;
     private static double lastErrorsCountPerDay = 0.0;
 
-    private static class CalculateKIntermediateResultRecord
-    {
-        double leftSum;
-        double rightSum;
-        double deltaSum;
-        double k;
+    private static void calculateKAndGatherStatisticTable(HSSFWorkbook lastModifiedWorkbook, Map<Integer, Integer> inputValues){
 
-        public CalculateKIntermediateResultRecord(double leftSum, double rightSum, double k) {
-            this.leftSum = leftSum;
-            this.rightSum = rightSum;
-            this.deltaSum = Math.abs(rightSum - leftSum);
-            this.k = k;
-        }
-
-        public double getLeftSum() {
-            return leftSum;
-        }
-
-        public double getRightSum() {
-            return rightSum;
-        }
-
-        public double getDeltaSum() {
-            return deltaSum;
-        }
-
-        public double getK() {
-            return k;
-        }
-    }
-
-    private static double calculateA(Map<Integer, Integer> inputValues, double calculatedB){
-        AtomicInteger sumDays = new AtomicInteger();
-        AtomicReference<Double> sumErrorsPerTime = new AtomicReference<>(0.0);
-
-        inputValues.forEach((curTime, curErrors) -> {
-            sumDays.addAndGet(curTime);
-            sumErrorsPerTime.updateAndGet(v -> v + curErrors);
-        });
-
-        double numerator = sumErrorsPerTime.get() - calculatedB * sumDays.get();
-
-        return numerator / 7;
-    }
-
-    private static double calculateB(Map<Integer, Integer> inputValues){
-
-        AtomicReference<Double> diminutiveInNumerator = new AtomicReference<>(0.0);
-        AtomicInteger sumDays = new AtomicInteger();
-        AtomicReference<Double> sumErrorsPerTime = new AtomicReference<>(0.0);
-        AtomicInteger sumSquareDays = new AtomicInteger();
-
-        inputValues.forEach((curTime, curErrors) -> {
-            diminutiveInNumerator.updateAndGet(v -> v + curTime * curErrors);
-            sumDays.addAndGet(curTime);
-            sumErrorsPerTime.updateAndGet(v -> v + curErrors);
-            sumSquareDays.addAndGet((int) Math.pow(curTime, 2));
-        });
-
-        double numerator = 7 * diminutiveInNumerator.get() - sumDays.get() * sumErrorsPerTime.get();
-        double denominator = 7 * sumSquareDays.get() - Math.pow(sumDays.get(), 2);
-
-        return numerator/denominator;
-    }
-
-    private static HSSFWorkbook calculateKAndGatherStatisticTable(Map<Integer, Integer> inputValues){
-
-        List<CalculateKIntermediateResultRecord> calculateKStatistic = new ArrayList<>();
-
-        double k = Consts.K;
+        double k = Consts.START_K;
 
         double leftSum;
         double rightSum;
-        double deltaSum;
 
         double iterNumber = 0.0;
+
+        // Create sheet with 'Statistic' name
+        HSSFSheet sheet = lastModifiedWorkbook.createSheet("Statistic");
+
+        int rowNum = 0;
+
+        // Headers
+        Row row = sheet.createRow(rowNum);
+        row.createCell(0).setCellValue("Iteration number");
+        row.createCell(1).setCellValue("Left sum");
+        row.createCell(2).setCellValue("Right sum");
+        row.createCell(3).setCellValue("Delta sum");
+        row.createCell(4).setCellValue("Intermediate K");
+
+        ++rowNum;
+
         while (true){
 
             ++iterNumber;
             leftSum = calculateLeftSum(inputValues, k);
             rightSum = calculateRightSum(inputValues, k);
 
-            calculateKStatistic.add(new CalculateKIntermediateResultRecord(leftSum, rightSum, k));
+            Row currentRow = sheet.createRow(rowNum);
+            currentRow.createCell(0).setCellValue(rowNum);
+            currentRow.createCell(1).setCellValue(calculateLeftSum(inputValues, k));
+            currentRow.createCell(2).setCellValue(calculateRightSum(inputValues, k));
+            currentRow.createCell(3).setCellValue(leftSum - rightSum);
+            currentRow.createCell(4).setCellValue(k);
+            ++rowNum;
 
-            deltaSum = leftSum - rightSum;
-
-            if (Math.abs(deltaSum) < Consts.EPSILON) {
+            if (Math.abs(leftSum - rightSum) < Consts.EPSILON) {
                 break;
             }
 
-            k = changeKValue(leftSum, rightSum, k, iterNumber); // Why K changing if we have correct delta already?
+            k = changeKValue(leftSum, rightSum, k, iterNumber);
         }
         System.out.println(iterNumber);
 
         calculatedK = k;
 
-        return gatherStatisticForKCalculation(calculateKStatistic);
+        calculateAndCacheN0(inputValues, calculatedK);
     }
 
     private static double calculateLeftSum(Map<Integer, Integer> inputValues, double intermediateK){
@@ -125,7 +74,6 @@ public class Calculator {
         for (Map.Entry<Integer, Integer> current : inputValues.entrySet()) {
 
             double exp = Math.pow(Math.E, intermediateK * current.getKey() * (-1));
-            //sum += current.getValue() * current.getKey() * exp; // [dmso0321] e^(-Kt) * n * t: are you sure?
             sum += current.getValue() * exp;
         }
 
@@ -143,13 +91,12 @@ public class Calculator {
             double exp1 = Math.pow(Math.E, intermediateK * current.getKey() * (-1)); // e^(-Kt)
             double exp2 = Math.pow(Math.E, intermediateK * current.getKey() * (-2)); // e^(-2Kt)
 
-            // sumNumerator1 += current.getValue() * exp1; // [dmso0321] n*e^(-Kt) I don't think so...
-            sumNumerator1 += current.getValue() * exp1 * current.getKey(); // [dmso0321] n * e^(-Kt) * t
-            /*sumNumerator2*/sumDenominator += current.getKey() * exp2;   // e^(-2Kt) * t
-            /*sumDenominator*/sumNumerator2 += exp2; // e^(-2Kt)
+            sumNumerator1 += current.getValue() * exp1 * current.getKey();
+            sumDenominator += current.getKey() * exp2;
+            sumNumerator2 += exp2;
         }
 
-        return (sumNumerator1 * sumNumerator2) / sumDenominator; // [dmso0321] Why? {[n*e^(-Kt)] * [t * e^(-2Kt)]}/[e^(-2Kt)] I think, it's not correct
+        return (sumNumerator1 * sumNumerator2) / sumDenominator;
 
 
     }
@@ -177,39 +124,6 @@ public class Calculator {
 
         denumerator *= resultK;
         calculatedN0 = numerator/denumerator;
-    }
-
-    // Statistic of K calculation
-    private static HSSFWorkbook gatherStatisticForKCalculation(List<CalculateKIntermediateResultRecord> calculateKStatistic){
-        // Excel file
-        HSSFWorkbook workbook = new HSSFWorkbook();
-
-        // Create sheet with 'Statistic' name
-        HSSFSheet sheet = workbook.createSheet("Statistic");
-
-        AtomicInteger rowNum = new AtomicInteger();
-
-        // Headers
-        Row row = sheet.createRow(rowNum.get());
-        row.createCell(0).setCellValue("Iteration number");
-        row.createCell(1).setCellValue("Left sum");
-        row.createCell(2).setCellValue("Right sum");
-        row.createCell(3).setCellValue("Delta sum");
-        row.createCell(4).setCellValue("Intermediate K");
-
-        rowNum.incrementAndGet();
-
-        calculateKStatistic.forEach(currentRecord -> {
-            Row currentRow = sheet.createRow(rowNum.get());
-            currentRow.createCell(0).setCellValue(rowNum.get());
-            currentRow.createCell(1).setCellValue(currentRecord.getLeftSum());
-            currentRow.createCell(2).setCellValue(currentRecord.getRightSum());
-            currentRow.createCell(3).setCellValue(currentRecord.getDeltaSum());
-            currentRow.createCell(4).setCellValue(currentRecord.getK());
-            rowNum.incrementAndGet();
-        });
-
-        return workbook;
     }
 
     // Table for graph of changes in the number of detected errors over time
@@ -281,6 +195,40 @@ public class Calculator {
 
     }
 
+    private static double calculateA(Map<Integer, Integer> inputValuesWithZero, double calculatedB){
+        AtomicInteger sumDays = new AtomicInteger();
+        AtomicReference<Double> sumErrorsPerTime = new AtomicReference<>(0.0);
+
+        inputValuesWithZero.forEach((curTime, curErrors) -> {
+            sumDays.addAndGet(curTime);
+            sumErrorsPerTime.updateAndGet(v -> v + curErrors);
+        });
+
+        double numerator = sumErrorsPerTime.get() - calculatedB * sumDays.get();
+
+        return numerator / Consts.CURRENT_DAYS_COUNT;
+    }
+
+    private static double calculateB(Map<Integer, Integer> inputValuesWithZero){
+
+        AtomicInteger diminutiveInNumerator = new AtomicInteger();
+        AtomicInteger sumDays = new AtomicInteger();
+        AtomicInteger sumErrorsPerTime = new AtomicInteger();
+        AtomicInteger sumSquareDays = new AtomicInteger();
+
+        inputValuesWithZero.forEach((curTime, curErrors) -> {
+            diminutiveInNumerator.updateAndGet(v -> v + curTime * curErrors);
+            sumDays.addAndGet(curTime);
+            sumErrorsPerTime.updateAndGet(v -> v + curErrors);
+            sumSquareDays.addAndGet((int) Math.pow(curTime, 2));
+        });
+
+        double numerator = Consts.CURRENT_DAYS_COUNT * diminutiveInNumerator.get() - sumDays.get() * sumErrorsPerTime.get();
+        double denominator = Consts.CURRENT_DAYS_COUNT * sumSquareDays.get() - Math.pow(sumDays.get(), 2);
+
+        return numerator/denominator;
+    }
+
     private static void createLinearInterpolationTable(HSSFWorkbook lastModifiedWorkbook, Map<Integer, Integer> inputValues){
 
         AtomicReference<Double> discrepancySquaresSum = new AtomicReference<>(0.0);
@@ -333,7 +281,7 @@ public class Calculator {
     }
 
     private static void createDebuggingCompletionPredictionTable(HSSFWorkbook lastModifiedWorkbook, double probabilityGoal){
-        // Create sheet with 'Debugging completion time prediction' name
+
         HSSFSheet sheet = lastModifiedWorkbook.createSheet("Debugging completion time prediction");
 
         int rowNum = 0;
@@ -348,7 +296,7 @@ public class Calculator {
         ++rowNum;
 
         double currentProbability = 0.0;
-        int predictableDaysCountForDebuggingCompletion = 7;
+        int predictableDaysCountForDebuggingCompletion = Consts.CURRENT_DAYS_COUNT;
 
         while (probabilityGoal > currentProbability)
         {
@@ -359,7 +307,6 @@ public class Calculator {
                 System.out.println("Very long debugging... Please reduce the probability of no error");
             }
         }
-
 
         row = sheet.createRow(rowNum);
         row.createCell(0).setCellValue(lastErrorsCountPerDay);
@@ -377,14 +324,15 @@ public class Calculator {
     }
 
     // Creates full Excel file with some sheets
-    public static void createReport(Map<Integer, Integer> inputValues){
+    public static void createReport(Map<Integer, Integer> inputValues, double probabilityGoal){
         // Excel file
-        HSSFWorkbook workbook = calculateKAndGatherStatisticTable(inputValues);
-        calculateAndCacheN0(inputValues, calculatedK);
+        HSSFWorkbook workbook = new HSSFWorkbook();
+
+        calculateKAndGatherStatisticTable(workbook, inputValues);
         createErrorsPerTimeGraphTable(workbook, inputValues);
         createLSMTable(workbook, inputValues);
         createLinearInterpolationTable(workbook,inputValues);
-        createDebuggingCompletionPredictionTable(workbook, 0.999);
+        createDebuggingCompletionPredictionTable(workbook, Consts.NO_ERRORS_PROBABILITY_GOAL);
 
         // Save excel
         try (FileOutputStream out = new FileOutputStream(new File(PathConsts.PATH_TO_SAVE_REPORT_FILE))) {
--- src/main/java/Consts.java
+++ src/main/java/Consts.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public interface Consts {
 
-    double K = 0.25;
+    double START_K = 0.25;
     double EPSILON = 0.001;
 
     Map<Integer, Integer> ERRORS_PER_INTERVAL_MAP = ImmutableMap.<Integer, Integer>builder()
@@ -16,5 +16,7 @@ public interface Consts {
             .put(7, 0)
             .build();
 
-    int MAX_PREDICTABLE_DAYS_COUNT_FOR_DEBUGGING_COMPLETION_BORDER = 500;
+    int CURRENT_DAYS_COUNT = ERRORS_PER_INTERVAL_MAP.size();
+    double NO_ERRORS_PROBABILITY_GOAL = 0.999;
+    int MAX_PREDICTABLE_DAYS_COUNT_FOR_DEBUGGING_COMPLETION_BORDER = 100;
 }
